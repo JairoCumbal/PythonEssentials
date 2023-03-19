bac40d37b415b4f51ee321820f0630f8364851b6
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 18:12:34 -0500

    Add files via upload

---
+++ ScriptsPython/Uso de variables.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 18:06:10 2023
+
+@author: Client
+"""
+
+#Uso de Variables
+
+num1=39 #Variable num1 con valor entero
+num2=13 #Variable num2 con valor entero
+suma=num1+num2 #Variable suma guarda el resultado de num1+num2
+print(suma) #Impresión de variable suma
+print("\n"*2) #Salto de línea (x2)
+div=num1/num2 #Variable div guarda el resultado de num1/num2
+print(div) #Impresión de la variable div
+divint=num1//num2 #Variable divint guarda el resultado en enteros de num1/num2
+print(divint) #Impresión de la variable divint
+x=5 #Declaro una variable entera
+letra="Python " #Declaro una variable string
+resultado=letra*x #Operación con las variables x y letra se guarda en variable resultado
+print(resultado) #Impresión de la variable resultado
