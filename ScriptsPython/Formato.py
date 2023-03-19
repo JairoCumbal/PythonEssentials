be4d4e8425b3a21ed399114a5ea2ce6a0766885c
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 18:40:43 -0500

    Add files via upload

---
+++ ScriptsPython/Formato.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 18:29:31 2023
+
+@author: Client
+"""
+
+#Formato
+
+pi=22/7
+print(pi)
+print("{:.2f}".format(pi))
+print("{:.10f}".format(pi))
+print("{:.50f}".format(pi))
+print("{:.51f}".format(pi)) #Hasta 51 flotantes, a partir del 52 se llena con cero
