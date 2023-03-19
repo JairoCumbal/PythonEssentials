7284986ba4691413e9f9149e8b8f2def710bfe06
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 18:05:07 -0500

    Add files via upload

---
+++ ScriptsPython/Operadores booleanos y matemáticos.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 18:03:50 2023
+
+@author: Client
+"""
+
+#Operadores booleanos y matemáticos
+
+print(4<5)
+print(4>4)
+print(4!=5)
+print(4==5)
+print(4<=5)
+print(4>=5)
