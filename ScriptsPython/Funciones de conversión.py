ae41a46db46fbb3325b3ba808a456b82e8624b64
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 18:29:05 -0500

    Add files via upload

---
+++ ScriptsPython/Funciones de conversión.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 18:25:46 2023
+
+@author: Client
+"""
+
+#Funciones de conversión
+x=5
+str1="Python"
+str2="Essentials"
+a=int()
+b=float()
+c=str()
+d=bool()
+
+print(type(x))
+print("El valor de x es: "+str(x)) #Para imprimir convierto la variable x de int a string
+print(type(x))
+x=str(x) #Asignar la conversión de una variable
+print(type(x))
+print(str1+str2) #Concatenación
+print("El valor de x es: ", x) #Imprimir dos valores distintos
