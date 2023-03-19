a5e2b1d93d1c00f66470d021adefc46622977671
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 18:58:12 -0500

    Add files via upload

---
+++ ScriptsPython/Uso de tuplas.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 18:53:47 2023
+
+@author: Client
+"""
+
+#Uso de tuplas
+
+tupla=("1",2,3.7,False,"b") #para declarar una tupla va entre paréntesis ()
+print(tupla[1]) #Imprimir una posición de la tupla
+#tupla[4]="4" no se puede cambiar el valor de una tupla porque es inmutable
+#del tupla[4] #No se puede eliminar le valor de una tubla
