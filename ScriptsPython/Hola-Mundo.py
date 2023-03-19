c1414b7964c30666384d70da80bde5d2596322db
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 17:34:45 -0500

    Add files via upload

---
+++ ScriptsPython/Hola-Mundo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 17:06:57 2023
+
+@author: Client
+"""
+#Impresión de un string
+
+print("hola mundo") #Función Print
+print("hola mundo")
+print("hola mundo")
+#print("hola mundo1")
+#print("hola mundo2")
