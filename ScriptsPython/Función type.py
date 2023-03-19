69c3fa9563c9cf2798f1e08b4bfc30ad5f29e8c2
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 17:50:35 -0500

    Add files via upload

---
+++ ScriptsPython/Función type.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 19 17:47:39 2023
+
+@author: Client
+"""
+
+#Función TYPE
+print(type(True)) #Función para imprimir el tipo de dato que reconoce Python print(type(argumento))
+print(type(1))
+print(type("1"))
+print(type(3.4))
