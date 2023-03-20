7beed4b3c0f58437305a6039b7b4c8a25d426955
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 19:14:39 -0500

    Add files via upload

---
+++ ScriptsPython/condicional if.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 19:18:33 2023
+
+@author: Client
+"""
+
+#Condicional if-else
+
+datos=10 #declaro una variable entera
+nativa=50 #declaro una variable entera
+if datos==nativa: #uso de if para condición
+    print("Las VLAN son iguales") #Impresión para verdadero
+else:
+    print("Las VLAN son diferentes") #Impresión para falso
