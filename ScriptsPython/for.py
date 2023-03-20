fab53c12f65a5f922421a820fc00d601d563e26b
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 19:40:14 -0500

    Add files via upload

---
+++ ScriptsPython/for.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 20:03:02 2023
+
+@author: Client
+"""
+
+lista=["R1","R2","R3",
+       "S1","S2","S3"] #Declaro una lista
+print(len(lista)) #Imprimo tamaño de la lista
+
+for item in lista:
+    print(item,end=" * ") #Mediante el uso de for se imprime cada posición de la lista y se termina con un string
+    
+
+
+    
