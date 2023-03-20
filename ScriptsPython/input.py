6081c587d925455ab09b41019efd9becfbbe29a1
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-19 19:07:33 -0500

    Add files via upload

---
+++ ScriptsPython/input.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 18:16:59 2023
+
+@author: Client
+"""
+
+#Uso de input
+
+#dato=input("Ingrese un dato: ")
+
+"""dato1=int(input("Ingrese un dato 1: "))
+dato2=int(input("Ingrese un dato 2: "))
+resultado=dato1+dato2
+print("El resultado de sumar ",dato1," + ",dato2," es: ",resultado)"""
+
+
+dato1=int(input("Ingrese un dato 1: ")) #Obtener dato1 de tipo entero con función input
+dato2=int(input("Ingrese un dato 2: ")) #Obtener dato2 de tipo entero con función input
+resultado=dato1+dato2 #Suma de dato1 más dato2
+print("El resultado de sumar ",dato1," + ",dato2," es: ",resultado) #Impresión y concatenación
