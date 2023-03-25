c0881209549ead6e23fce5b98a40dbb2b8eaec01
JairoCumbal <128174607+JairoCumbal@users.noreply.github.com>
2023-03-24 21:31:50 -0500

    Add files via upload

---
+++ ScriptsPython/uso de clases - POO/Clases en Python-2.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 18:43:07 2023
+
+@author: Client
+"""
+
+class Employee:
+   'Common base class for all employees'
+   empCount = 0
+   #Método constructor
+   def __init__(self,name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print ("Total Employee %d", Employee.empCount)
+
+   def displayEmployee(self):
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+"This would create first object of Employee class"      
+emp1 = Employee("Zara", 2000)
+"This would create second object of Employee class"
+emp2 = Employee("Sandra", 5000)
+emp3 = Employee("Ana",8000)
+emp4 = Employee("Briguitte",8000)
+emp5 = Employee ('Juan',1200)
+emp6 = Employee ('Carlos',1600)
+emp7=Employee("Mirkka", 2500)
+emp8=Employee(5888, 2500)
+emp9=Employee("Jairo", 1875)
+emp10=Employee("Paúl", 1650)
+emp1.displayEmployee()
+emp2.displayEmployee()
+emp3.displayEmployee()
+emp4.displayEmployee()
+emp5.displayEmployee()
+emp9.displayEmployee()
+emp10.displayEmployee()
+print ("Total Employee %d" % Employee.empCount)
---
+++ ScriptsPython/uso de clases - POO/Clases en Python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 18:17:51 2023
+
+@author: Client
+"""
+#Creación de clase
+class Casa:
+    pass
+#Creación de objetos
+obj1=Casa()
+obj2=Casa()
